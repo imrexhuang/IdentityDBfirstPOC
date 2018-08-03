154514f044b512dec407cbbe1e5c49f682ecb257
Rex <im@rex.tw>
2018-08-03 12:30:04 +0800

    init commit

---
+++ IdentityDBfirstPOC/Models/ViewModels/SpendingTrackerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityDBfirstPOC.Models.ViewModels
+{
+    public class SpendingTrackerViewModel
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [DisplayName("類別")]
+        public int TYPE { get; set; }
+
+        [DisplayName("日期")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DATE { get; set; }
+
+        [DisplayName("金額")]
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = false)]
+        public int AMOUMT { get; set; }
+
+        [DisplayName("備註")]
+        public String REMARK { get; set; }
+    }
+
+    //public enum TypeEnum
+    //{
+    //    [Display(Name = "收入")]
+    //    收入 = 'D',
+
+    //    [Display(Name = "支出")]
+    //    支出 = 'C'
+    //}
+}
---
+++ IdentityDBfirstPOC/Repository/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using IdentityDBfirstPOC.Models;
+
+namespace IdentityDBfirstPOC.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public UnitOfWork()
+        {
+            Context = new ModelContext(); //自訂的DbContext
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+
+
+    }
+}
