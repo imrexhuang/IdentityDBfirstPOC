f413ad51b39be244d3df6e75e6c73a95bbba1a45
Rex <im@rex.tw>
2018-08-11 20:22:22 +0800

    編輯、修改、刪除 要Admin角色才顯示URL User.IsInRole("Admin")

--- IdentityDBfirstPOC/Helper/AccountBookHelper.cs
+++ IdentityDBfirstPOC/Helper/AccountBookHelper.cs
@@ -22,5 +22,11 @@ namespace IdentityDBfirstPOC.Helper
             }
             
         }
+
+        // https://stackoverflow.com/questions/6981853/asp-net-mvc3-razor-display-actionlink-based-on-user-role
+        public static MvcHtmlString If(this MvcHtmlString value, bool evaluation)
+        {
+            return evaluation ? value : MvcHtmlString.Empty;
+        }
     }
 }
